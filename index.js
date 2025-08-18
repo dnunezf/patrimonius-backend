f03d19007f8303f1549b846c1dbac6cd90d14ffa
gabito <herreragabriel1408@gmail.com>
2025-08-18 17:31:26 -0600

    Backend funcional

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const cors = require('cors');
-
 const app = express();
+const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Patrimonius backend activo');
+app.post('/formulario', (req, res) => {
+  console.log('Datos recibidos:', req.body);
+  res.json({ mensaje: 'Datos recibidos correctamente', datos: req.body });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en puerto ${PORT}`);
+  console.log(`Servidor Express corriendo en puerto ${PORT}`);
 });
