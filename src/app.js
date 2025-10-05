8bf6ee9d4d9ae278f882d12439a3dbc5f664b644
dnunezf <david.a.nunez27@outlook.com>
2025-10-05 15:10:39 -0600

    Rebase Gabo changes

--- src/app.js
+++ src/app.js
@@ -25,6 +25,9 @@ export const app = express();
 export const logger = pino();
 
 app.use(cors());
+//app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(express.json());
 
 // Rutas públicas
