57a6770f875a5366587cac4306937081d2f09a23
WillyRC2001 <willyrc2001@gmail.com>
2025-10-05 14:58:30 -0600

    Aumento el tamaño de archivos importados

--- src/app.js
+++ src/app.js
@@ -25,7 +25,9 @@ export const app = express();
 export const logger = pino();
 
 app.use(cors());
-app.use(express.json());
+//app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Rutas públicas
 app.use("/health", healthRoutes);
