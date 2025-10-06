1292a81c62253393ea3d7d97e2e3d84f443417d2
Gabo Herrera <herreragabriel1408@gmail.com>
2025-10-06 17:54:20 -0600

    incluido lo de david tam archivos a 500mb

--- src/app.js
+++ src/app.js
@@ -27,8 +27,8 @@ export const logger = pino();
 
 app.use(cors());
 //app.use(express.json());
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.json({ limit: "500mb" }));
+app.use(express.urlencoded({ limit: "500mb", extended: true }));
 
 // Rutas públicas
 app.use("/health", healthRoutes);
