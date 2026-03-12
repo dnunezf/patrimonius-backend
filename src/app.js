012bc0608edff76ef95670f15a476176f6386df0
LittleHehet <alexia.alvarado18@gmail.com>
2026-03-12 14:35:18 -0600

    clases firma vieja alexia
    borrar clases viejas, usar solo las de kendra

--- src/app.js
+++ src/app.js
@@ -40,10 +40,6 @@ import { buildConfidentialityRoutes } from "./routes/confidentiality.routes.js";
 import { ConfidentialityRepo } from "./repositories/confidentiality.repo.js";
 import { ConfidentialityService } from "./services/confidentiality.service.js";
 
-//HU-020
-import firmaExternalRouter from "./routes/firmaExternal.routes.js";
-
-
 export const app = express();
 export const logger = pino();
 
@@ -100,8 +96,6 @@ app.use("/", documentMetadataRoutes);
 
 app.use("/permissions", authGuard, permissionRouter);
 app.use("/notificacion", authGuard, notificacionRouter);
-
-app.use("/firma", firmaExternalRouter);
 
 // Optional startup sync
 if (process.env.SEED_PLANTILLAS === "true") {
