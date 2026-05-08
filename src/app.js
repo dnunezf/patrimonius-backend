edad394d2a5fa4d009b3252b391dba8ed5eef49e
Kendra Artavia Caballero <kendra.artavia.caballero@gmail.com>
2026-05-08 16:32:20 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -202,7 +202,6 @@ app.use("/", comentariosRoutes);
 app.use("/indices", indiceRouter);
 // app.use("/uploads", express.static(path.resolve(process.cwd(), "uploads")));
 app.use("/uploads", express.static(getUploadsRoot()));
-app.use("/uploads", express.static(UPLOADS_ROOT));
 app.use("/api/series", authGuard, serieRoutes);
 app.use("/subseries", authGuard, subserieRoutes);
 app.use("/api/unidades", unidadOrganizacionalRoutes);
